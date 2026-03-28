12669b936840f87463d07265427eff751589844c
noi <email@7-de.de>
2026-03-28 14:49:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
 const fs = require("fs");
-
 const fetch = require("cross-fetch");
 const { Socket } = require("net");
 
-async function readFileBytesRange(start, end, filepath) {
-    const readStream = fs.createReadStream(filepath, { start, end });
-    return await new Promise(resolve => {
-        let res = "";
-        readStream.on("data", chunk => {
-            res += chunk;
-        });
-        readStream.on("end", () => {
-            console.log(res.length);
-            resolve(res);
-            console.log("read finished")
-        });
-        readStream.on("error", err => {
-            console.error(err)
-        })
-    });
-
-}
-
-
+const _GRAPH_API_URL = `https://graph.microsoft.com/v1.0`;
 async function check_mics_server_connectivty() {
     const res_list = ([await check_hostname_delay_ms("graph.microsoft.com"), await check_hostname_delay_ms("login.microsoftonline.com"),]);
     const res_list_alias_name = ['graph.microsoft.com：      ', 'login.microsoftonline.com：']
@@ -59,13 +39,12 @@ async function check_hostname_delay_ms(host, port = 443) {
     };
     return return_res;
 }
-
 module.exports = class Main {
     constructor({ od_code, client_secret, ...obj }) {
         this.od_code = od_code;
         this.client_secret = client_secret;
-        this.init();
         this.refresh_token = obj['refresh_token'];
+        // this.init();
     }
     async init() {
         await this.check_server_connectivity();
@@ -74,7 +53,7 @@ module.exports = class Main {
         } else {
             await this.get_refresh_token();
         };
-        this.download(`backups/10938.exe`)
+        // this.download(`backups/10938.exe`)
 
     }
     _JSON_PARSE(string) {
@@ -131,8 +110,8 @@ module.exports = class Main {
     }
     async put_upload_url(url, chunk_size, chunk_range, file_size, chunk) {
         console.log({
-                   "content-length": chunk_size,
-                "content-range": `bytes ${chunk_range}/${file_size}`,
+            "content-length": chunk_size,
+            "content-range": `bytes ${chunk_range}/${file_size}`,
         })
         const res = await fetch(url, {
             method: "PUT",
@@ -145,7 +124,7 @@ module.exports = class Main {
         return (await res.json());
     }
     async create_upload_session(filepath_upload, filename_upload, file_size, file_path) {
-        const res = await fetch(`https://graph.microsoft.com/v1.0/drive/root:/${filepath_upload}${filename_upload}:/createUploadSession`, {
+        const res = await fetch(`${_GRAPH_API_URL}/drive/root:/${filepath_upload}${filename_upload}:/createUploadSession`, {
             method: "POST",
             headers: {
                 'content-type': "application/json",
@@ -160,123 +139,136 @@ module.exports = class Main {
         });
         const res_json = (await res.json());
         console.log(res_json);
-        const loop_count = Math.ceil(file_size / 10485760);
+        const loop_count = Math.ceil(file_size / 10485760); /* 10mb */
         let start = 0;
         let end = 10485760;
         let contentLength = 10485761;
         let urls = res_json['uploadUrl'];
-        for(let i =0; i<loop_count;i++){
+        for (let i = 0; i < loop_count; i++) {
             let res_d = await this.put_upload_url(
-            urls,
-            contentLength,
-            `${start}-${end}`,
-            file_size,
-            fs.createReadStream(file_path,{start,end})
+                urls,
+                contentLength,
+                `${start}-${end}`,
+                file_size,
+                fs.createReadStream(file_path, { start, end })
             );
             console.log(res_d);
-            start = end+1;
-            if(parseInt(file_size) - start < 10485760){
+            start = end + 1;
+            if (parseInt(file_size) - start < 10485760) {
                 console.log("complete file");
-                console.log(parseInt(file_size)-start)
-                end+=parseInt(file_size)-start;
+                console.log(parseInt(file_size) - start)
+                end += parseInt(file_size) - start;
                 contentLength = parseInt(file_size) - start;
-            }else{
+            } else {
 
-                end=start+10485760;
+                end = start + 10485760;
             }
         }
 
-        
+
     }
-    async copy(item_id,parentReference){
-        const res = await fetch(`/me/drive/items/{item-id}/copy`,{
-            method:"POST",
-            headers:{
-                "content-type":'application/json'
+    async copy(item_id, parentReference) {
+        const res = await fetch(`${_GRAPH_API_URL}/me/drive/items/${item_id}/copy`, {
+            method: "POST",
+            headers: {
+                 "Authorization": `bearer ${this.access_token}`,
+                "content-type": 'application/json',
             },
-            body:JSON.stringify({
-                /* 
-  "parentReference": {
-    "driveId": "6F7D00BF-FC4D-4E62-9769-6AEA81F3A21B",
-    "id": "DCD0D3AD-8989-4F23-A5A2-2C086050513F"
-  },
-  "name": "contoso plan (copy).txt" */
-            })}
+            body: JSON.stringify({
+                parentReference,
+ /*  "parentReference": {
+    "driveId": "0F7A41909C0DDBB3",
+    "id": "F7A41909C0DDBB3!seed701c5dada49ffa3caaab9c4968832"
+  }, */
+  "name": "新网站文件夹备份" 
+            })
+        }
         );
         console.log(await res.text())
     }
-    async mkdir(dirname){
-        const res = await fetch(`/me/drive/root/children`,{
-            method:"POST",
-            headers:{
-                "content-type":"application/json"
+    async mkdir(dirname,parent_item_id="") {
+        const res = await fetch(`${_GRAPH_API_URL}/me/drive/${parent_item_id === "" ? 'root' : `items/${parent_item_id}`}/children`, {
+            method: "POST",
+            headers: {
+                   "Authorization": `bearer ${this.access_token}`,
+                "content-type": 'application/json',
             },
-            body:JSON.stringify({
-                /* {
-  "name": "New Folder",
-  "folder": { },
-  "@microsoft.graph.conflictBehavior": "rename"
-} */
+            body: JSON.stringify({
+                "name": dirname,
+                "folder": { },
+                "@microsoft.graph.conflictBehavior": "rename"
             })
         });
         console.log(await res.text());
 
     }
-    async move(item_id,new_path_id,item_name){
-        const res = await fetch(`/me/drive/items/{item-id}`,{
-            method:"PATCH",
-            headers:{
-                "content-type":'application/json',
+    async move(item_id, new_path_id, item_name) {
+        const res = await fetch(`${_GRAPH_API_URL}/me/drive/items/${item_id}`, {
+            method: "PATCH",
+            headers: {
+                "Authorization": `bearer ${this.access_token}`,
+                "content-type": 'application/json',
             },
-            body:JSON.stringify({
+            body: JSON.stringify({
                 "parentReference": {
-                    "id": item_id
+                    "id": new_path_id,
                 },
                 "name": item_name
-                            })
+            })
         });
         console.log(await res.text());
 
     }
-    async search(query){
-        const res = await fetch(`/me/drive/root/search(q='{search-query}')`,{
-            method:"GET",
-            headers:{
+    async search(query) {
+        const res = await fetch(`${_GRAPH_API_URL}/me/drive/root/search(q='${query}')`, {
+            method: "GET",
+            headers: {
+                           "Authorization": `bearer ${this.access_token}`,
+                "content-type": 'application/json',
                 /* authorization here  */
                 // "content-type":
             },
         });
         console.log(await res.text());
     }
-    async delete(item_id){
-        const res  = await fetch(` /me/drive/items/{item-id}`,{
-            method:"DELETE",
-            headers:{
-                /* authotication here */
+    async delete(item_id) {
+        const res = await fetch(`${_GRAPH_API_URL}/me/drive/items/${item_id}`, {
+            method: "DELETE",
+            headers: {
+                 "Authorization": `bearer ${this.access_token}`,
+                // "content-type": 'application/json',
             },
 
         });
         console.log(await res.text());
-        
+
     }
-    async item_info(){}
-    async download(path){
-        const res = await fetch(`https://graph.microsoft.com/v1.0/me/drive/root:/${path}:/content`,{
+    async item_info(item_path) {
+        const res = await fetch(`${_GRAPH_API_URL}/me/drive/root:/${item_path}`,{
             method:"GET",
             headers:{
-                                "Authorization": `bearer ${this.access_token}`
+                 "Authorization": `bearer ${this.access_token}`,
+            }
+        });
+        console.log(await res.json());
+     }
+    async download(path) {
+        const res = await fetch(`${_GRAPH_API_URL}/me/drive/root:/${path}:/content`, {
+            method: "GET",
+            headers: {
+                "Authorization": `bearer ${this.access_token}`
             }
         });
         const fs_stream = fs.createWriteStream("test.exe");
         const write_ = await res.body.pipe(fs_stream);
-        write_.on("finish",function(){
+        write_.on("finish", function () {
             fs_stream.close();
         })
-        
+
         console.log(res.headers)
     }
     async list(path = "") {
-        const res = await fetch(`https://graph.microsoft.com/v1.0/me/drive/root${path && `:/${path}:`}/children`, {
+        const res = await fetch(`${_GRAPH_API_URL}/me/drive/root${path && `:/${path}:`}/children`, {
             method: "GET",
             headers: {
                 "Authorization": `bearer ${this.access_token}`
